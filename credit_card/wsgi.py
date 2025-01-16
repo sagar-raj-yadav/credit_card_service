d8b403ca4ca1bb74357fa268f2996df151594bf2
sagar raj yadav <115137591+sagar-raj-yadav@users.noreply.github.com>
2025-01-16 11:29:03 +0530

    Update wsgi.py

--- credit_card/wsgi.py
+++ credit_card/wsgi.py
@@ -14,9 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'credit_card.settings')
 
 application = get_wsgi_application()
-app=application
-
-from app import app  # or from myapp.app import app
-
-if __name__ == "__main__":
-    app.run()
